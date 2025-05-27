2cf87f1f6958f9335ec2f76827dd50dc6b6fb9cf
tiakocedric <tiakocedric6@gmail.com>
2025-05-27 15:24:15 +0100

    Fix import errors and add missing dependency

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from '../components/ui/button';
+import { Button } from '../components/ui/Button';
 import { Calendar } from '../components/ui/calendar';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../components/ui/select';
 
